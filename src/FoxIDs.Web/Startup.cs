36045b0531c79c41d1f127b4565b5202ecf8fb35
Anders Revsgaard <anders@itfoxtec.com>
2021-01-22 13:54:53 +0100

    Cookie Policy

--- src/FoxIDs.Web/Startup.cs
+++ src/FoxIDs.Web/Startup.cs
@@ -1,14 +1,11 @@
 using FoxIDs.Web.Infrastructure.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FoxIDs.Web
 {
@@ -26,6 +23,13 @@ namespace FoxIDs.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationInsightsTelemetry(options => { options.DeveloperMode = CurrentEnvironment.IsDevelopment(); });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
             services.AddHsts(options =>
             {
@@ -51,6 +55,8 @@ namespace FoxIDs.Web
             app.UseHttpsRedirection();
             app.UseStaticFilesCacheControl(CurrentEnvironment);
             app.UseProxyClientIpMiddleware();
+
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
