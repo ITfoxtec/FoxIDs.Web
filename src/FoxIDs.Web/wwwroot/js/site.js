e5191a3e9a333faeeeefe0cc84177963c4bc8613
Anders Revsgaard <anders@itfoxtec.com>
2021-02-05 21:29:24 +0100

    js in function

--- src/FoxIDs.Web/wwwroot/js/site.js
+++ src/FoxIDs.Web/wwwroot/js/site.js
@@ -1,9 +1,15 @@
-﻿$(document).ready(function () {
-    $('.markdown-page h1, .markdown-page h2, .markdown-page h3, .markdown-page h4').each(function () {
+﻿/*! Site JS */
 
-        var id = $(this).attr('id');
-        $(this).addClass('anchor-heading');
-        var anchor = $('<a class="anchor-link" href="#' + id + '">#</a>');
-        $(this).append(anchor);
+(function () {
+    $(document).ready(function () {
+        $('.markdown-page h1, .markdown-page h2, .markdown-page h3, .markdown-page h4').each(function () {
+
+            var id = $(this).attr('id');
+            $(this).addClass('anchor-heading');
+            var anchor = $('<a class="anchor-link" href="#' + id + '">#</a>');
+            $(this).append(anchor);
+        });
     });
-});
+
+})();
+
